43a347e374b4dad5b99fe0611497668f8427f8a0
Mohit Singh Chauhan <mohit.chauhan2580@gmail.com>
2023-04-18 11:35:53 +0530

    bot done

---
+++ telegram_ecommerce/utils/consts.py
@@ -0,0 +1,17 @@
+from .utils import load_json_file
+
+credentials_path = "user_credentials.json"
+
+
+default_language = "en"
+currency = "USD"
+credentials_path = "telegram_ecommerce/utils/user_credentials.json"
+credentials = load_json_file(credentials_path)
+db_credentials = credentials["db_credentials"]
+provider_token = credentials["provider_token"]
+BAD_RATING = 0
+REGULAR_RATING = 5
+GOOD_RATING = 10
+
+
+
